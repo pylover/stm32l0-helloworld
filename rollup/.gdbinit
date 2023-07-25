fedbe570629a309156fe743734b39ae8e470e3de
pylover <vahid.mardani@gmail.com>
2023-07-25 09:21:21 +0330

    More siplification

--- rollup/.gdbinit
+++
@@ -1,2 +0,0 @@
-target extended-remote :3333
-monitor arm semihosting enable
--- scripts/rollup.sh
+++ scripts/rollup.sh
@@ -3,4 +3,4 @@
 HERE=`dirname "$(readlink -f "$BASH_SOURCE")"`
 mkdir -p ${HERE}/../build
 cd ${HERE}/../build
-cmake -DCMAKE_TOOLCHAIN_FILE=${HERE}/../rollup/crosstool.cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=${HERE}/../stm32/stm32.cmake ..
